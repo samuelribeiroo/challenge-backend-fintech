185daa703532a86a00953b5983d78499253a65e4
samuelribeiroo <samuelribeirodev@gmail.com>
2024-11-15 19:11:21 -0300

    feat(security api): enable cors and set rate limit

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,18 @@
 import { env } from "@/env"
+import { fastifyCors } from "@fastify/cors"
+import { fastifyRateLimit } from "@fastify/rate-limit"
 import { app } from "./app"
+
+app.register(fastifyCors, {
+  origin: env.NODE_ENV,
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+})
+
+app.register(fastifyRateLimit, {
+  max: 100,
+  timeWindow: "1 minute",
+})
 
 app
   .listen({
