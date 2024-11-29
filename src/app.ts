00541d96cfa85fdec446fb985c06e0a8bea2111f
samuelribeiroo <samuelribeirodev@gmail.com>
2024-11-29 16:20:24 -0300

    refactor(small changes): ternary rather than conditional

--- src/app.ts
+++ src/app.ts
@@ -7,17 +7,17 @@ export const app = fastify()
 
 app.register(userRoutes)
 
+// Global Error handler based on zod validation schema.
+
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
     reply.status(400).send({ message: "Erron while validating data.", issue: error.format() })
   }
 
-  if (env.NODE_ENV !== "prod") {
-    console.log(error)
-  } else {
-    // For the future whether be a prerequisite we can implement a external service to create a  monitoring Tool, like Sentry/Relic..
-  }
+  env.NODE_ENV !== "prod" ? console.log(error) : null
 
+  // For the future in some case whether be a prerequisite we can implement a external service to create a  monitoring tool, like Sentry/Relic/Data Dog..
+ 
   if (error.statusCode === 429) {
     return reply.status(429).send({ message: error.stack })
   }
