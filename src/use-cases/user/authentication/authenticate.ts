b74a20528616ef97da0b9d05b1ce98f06deb73f6
samuelribeiroo <samuelribeirodev@gmail.com>
2024-12-04 18:23:00 -0300

    feat(authenticate): use-case authenticate enabled

---
+++ src/use-cases/user/authentication/authenticate.ts
@@ -0,0 +1,20 @@
+import { AuthenticateUserRequest, UserResponse, AuthenticateUserResponse, IUser } from "@/models/IUser"
+import { IUserRepository } from "@/repositories/customer-repository"
+import { compare } from "bcrypt"
+import { InvalidCredentialsError } from "../../errors/invalid-credentials-error"
+
+export class AuthenticateUseCase {
+  constructor(private readonly customerRepository: IUserRepository) {}
+
+  async authenticate({ cpf, password }: AuthenticateUserRequest): Promise<AuthenticateUserResponse> {
+    const user = await this.customerRepository.findCpf(cpf) as IUser
+
+    if (!user) throw new InvalidCredentialsError()
+
+    const passwordMatches = await compare(password, user.password)
+
+    if (!passwordMatches) throw new InvalidCredentialsError()
+
+    return user
+  }
+}
