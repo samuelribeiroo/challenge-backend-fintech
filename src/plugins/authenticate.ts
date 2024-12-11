2910cd2ba6408839aba351d44985d83ac44c55e5
samuelribeiroo <samuelribeirodev@gmail.com>
2024-12-10 21:12:34 -0300

    bug(fix): fastify plugin updated

--- src/plugins/authenticate.ts
+++ src/plugins/authenticate.ts
@@ -1,15 +1,29 @@
-import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify"
+import { NoTokenProvidedError } from '@/use-cases/errors/no-token-provided-error'
+import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify'
+import fp from 'fastify-plugin'
 
-export default function authenticatePlugin(app: FastifyInstance) {
-  app.decorate("authenticate", async (request: FastifyRequest, reply: FastifyReply) => {
-    try {
-      await request.jwtVerify()
-    } catch (error) {
-      return reply.status(401).send({ message: "Unauthorized" })
-    }
-  })
+interface AuthenticatePluginOptions {
+  customErrorMessage?: string
 }
 
+export default fp((app: FastifyInstance, opts: AuthenticatePluginOptions) => {
+  app.decorate('authenticate', async (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+      const token = request.headers.authorization?.replace('Bearer ', '')
+
+      if (!token) throw new NoTokenProvidedError()
+
+      await request.jwtVerify()
+    } catch (error) {
+      if (error instanceof NoTokenProvidedError) reply.status(401).send({ message: error.message || opts.customErrorMessage }) 
+
+      return reply.status(500).send({ message: 'Internal Server Error' })
+    }
+  })
+})
+
 declare module "fastify" {
-  interface FastifyInstance { authenticate: any }
+  interface FastifyInstance {
+    authenticate: (request: FastifyRequest, reply: FastifyReply) => Promise<void>;
+  }
 }
